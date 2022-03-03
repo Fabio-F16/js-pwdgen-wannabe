61271140f82aff0212f68a199cdade3f108dd996
Fabio Filippini <fipsfabio95@gmail.com>
2022-03-03 14:08:42 +0100

    esercizio in console

---
+++ script.js
@@ -0,0 +1,13 @@
+const firstName = window.prompt('Qual è il tuo nome?')
+
+const lastName = window.prompt('Qual è il tuo cognome?')
+
+const preferColor = window.prompt('Qual è il tuo colore preferito?')
+
+const randomCode = 'n28w130z'
+
+// console.log(firstName + lastName + preferColor + randomCode)
+
+console.log(`${firstName}${lastName}${preferColor}${randomCode}`
+
+);
