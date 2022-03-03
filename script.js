a34daaf1495f097db785c44851fcc6b79ef5db46
Fabio Filippini <fipsfabio95@gmail.com>
2022-03-04 00:39:13 +0100

    utud

--- script.js
+++ script.js
@@ -6,12 +6,43 @@ const lastName = window.prompt('Qual è il tuo cognome?')
 
 const preferColor = window.prompt('Qual è il tuo colore preferito?')
 
-const randomCode = 'n28w130z'
-
-const passwordGenerator = firstName + lastName + preferColor + randomCode
-
-// console.log(firstName + lastName + preferColor + randomCode)
+const randomCode = Math.floor(Math.random() * 10000000000) + 1;
 
 console.log(`${firstName}${lastName}${preferColor}${randomCode}`);
+// console.log(firstName + lastName + preferColor + randomCode)
+
+
+const passwordGenerator = `${firstName}${lastName}${preferColor}${randomCode}`;
 
 document.getElementById('generator').innerText = passwordGenerator;
+
+
+
+
+
+
+
+
+
+
+
+
+// function getRandomString(length) {
+//     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//     var result = '';
+//     for (var i = 0; i < length; i++) {
+//         result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+//     }
+//     return result;
+// }
+
+
+
+
+
+
+
+
+
+
+
