4fe704ba753ae1aacd6cc49eca72836b6529d37a
Fabio Filippini <fipsfabio95@gmail.com>
2022-03-04 14:01:21 +0100

    post correzzione

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-alert('Sei pronto a generare la tua password?')
+alert('Sei pronto a generare la tua password?');
 
-const firstName = window.prompt('Qual è il tuo nome?')
+const firstName = window.prompt('Qual è il tuo nome?', 'Nome');
 
-const lastName = window.prompt('Qual è il tuo cognome?')
+const lastName = window.prompt('Qual è il tuo cognome?', 'Cognome');
 
-const preferColor = window.prompt('Qual è il tuo colore preferito?')
+const preferColor = window.prompt('Qual è il tuo colore preferito?', 'Colore');
 
 const randomCode = Math.floor(Math.random() * 10000000000) + 1;
 
-console.log(`${firstName}${lastName}${preferColor}${randomCode}`);
+// console.log(`${firstName}${lastName}${preferColor}${randomCode}`);
 // console.log(firstName + lastName + preferColor + randomCode)
 
 
 const passwordGenerator = `${firstName}${lastName}${preferColor}${randomCode}`;
+console.log(passwordGenerator)
 
 document.getElementById('generator').innerText = passwordGenerator;
 
